e1a6e949ddf8aaf53ca24ef2ce78f23248c54449
Natnael47 <nati47m10@gmail.com>
2024-12-15 13:34:53 +0300

    #3

--- user_frontend/src/App.jsx
+++ user_frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Login from './pages/login'
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
       <Routes>
-        <Route path='/' element={<login />} />
+        <Route path='/' element={<Login />} />
       </Routes>
     </div>
   )
